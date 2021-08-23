9644283440daa335c9e12396eabff4a4ae17124f
hzxgitgod <u456879@163.com>
2021-08-23 12:58:40 +0800

    add code to dojava file

--- dojava.py
+++ dojava.py
@@ -1,3 +1,9 @@
 # this is a java source code file.
 # Author : hzx
 # Date : 8/22/21
+packge com.hzx
+public class HelloJava{
+	public static void main(String[] args){
+		System.out.println("Hello Everybody,this is my first java Program");
+	}
+}
