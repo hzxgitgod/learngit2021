ffcbd4c72de1afdb89ecc7464ce78500629d3e7c
hzxgitgod <u456879@163.com>
2021-08-23 13:08:24 +0800

    add real java file

---
+++ HelloJava.java
@@ -0,0 +1,11 @@
+/**
+* this is a java source code file.
+* Author : hzx
+* Date : 8/22/21
+**/
+packge com.hzx
+public class HelloJava{
+	public static void main(String[] args){
+		System.out.println("Hello Everybody,this is my first java Program");
+	}
+}
